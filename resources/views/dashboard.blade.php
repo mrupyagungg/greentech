0985b70079cfb4e65c86b5aa632d6ad990332447
Mrupy Agung Pratama <116895857+mrupyagungg@users.noreply.github.com>
2024-03-22 20:59:19 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -51,7 +51,7 @@
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
-                                        Uang Keluar</div>
+                                        Uang </div>
                                     <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
                                 </div>
                                 <div class="col-auto">
