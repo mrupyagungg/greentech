2102e3d83d750191a0c120f177d419c918a49e9c
mrupyagungg <mrrupyagung@gmail.com>
2024-03-23 12:06:30 +0700

    update

--- resources/views/layoutadmin/sidebar.blade.php
+++ resources/views/layoutadmin/sidebar.blade.php
@@ -1,12 +1,4 @@
     <body>
-        <style>
-            h2,
-            span
-            hr{
-                color: black;
-            }
-        </style>
-
         <!--  Body Wrapper -->
         <div class="page-wrapper " id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
         data-sidebar-position="fixed" data-header-position="fixed">
